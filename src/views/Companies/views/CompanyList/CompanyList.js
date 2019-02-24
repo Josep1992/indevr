048272262eda61e53823419852fc1735f096d108
Andrea Reed <andrea@chasingatoms.com>
2019-02-24 09:50:45 -0700

    style table

--- src/views/Companies/views/CompanyList/CompanyList.js
+++ src/views/Companies/views/CompanyList/CompanyList.js
@@ -51,7 +51,7 @@ class CompanyList extends Component {
     const data = companies.get('results').map(company => ({
       id: company.get('id'),
       company: (
-        <div>
+        <div className="company-table__logo-cell">
           <img src={company.get('logo')} alt="company logo" className="img-responsive" /> {company.get('company_name')}
         </div>
       ),
@@ -69,13 +69,13 @@ class CompanyList extends Component {
     return (
       <ReactTable
         resizable={false}
-        ref="chatTable"
         data={data}
-        noDataText=""
+        className="-striped -highlight"
         columns={[
           {
             Header: 'Company',
             accessor: 'company',
+            width: 300,
           },
           {
             Header: 'Location',
@@ -84,14 +84,20 @@ class CompanyList extends Component {
           {
             Header: 'Contacts',
             accessor: 'contacts',
+            width: 80,
+            className: 'text-center',
           },
           {
             Header: 'Apps',
             accessor: 'apps',
+            width: 80,
+            className: 'text-center',
           },
           {
             Header: 'Saved',
             accessor: 'saved',
+            width: 80,
+            className: 'text-center',
           },
           {
             Header: 'Last Activity',
@@ -154,7 +160,21 @@ class CompanyList extends Component {
 
     return (
       <div className="container">
-        Companies
+        <div className="company-list__header">
+          <div className="company-list__header-text">
+            <h1>Companies</h1>
+          </div>
+          <div className="company-list__add-button-wrapper">
+            <button className="btn btn-primary" onClick={() => history.push('/company/add')}>
+              <Icon icon="Plus" /> New Company
+            </button>
+          </div>
+        </div>
+        <p className="company-list__helper-text">
+          Add companies you're interested in here. Companies can have multiple jobs associated with them, but don't have
+          to have any. Use this list to narrow down the places you'd like to work by researching the company, making
+          connections, and tracking available jobs.
+        </p>
         {this.renderTable()}
         {companies.get('total') > PAGE_SIZE && this.renderPagination()}
       </div>
